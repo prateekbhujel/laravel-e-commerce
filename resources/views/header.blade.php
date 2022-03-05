0949971da303fa966929b82c8796ded61608206d
Pratik Bhujel <prateekbhujelpb@gmail.com>
2022-03-05 09:27:28 +0545

    Login Logout and cart completed

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,26 +1,51 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Tika's</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<?php 
+use App\Http\Controllers\ProductController;
+$total=0;
+if(Session::has('user'))
+{
+  $total= ProductController::cartItem();
+}
+
+?>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/">E-Comm</a>
+      </div>
   
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Orders<span class="sr-only">(current)</span></a>
-          </li>
-        <form class="form-inline my-1 my-lg-1">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="/myorders">Orders</a></li>
+        </ul>
+        <form action="/search" class="navbar-form navbar-left">
+          <div class="form-group">
+            <input type="text" name="query" class="form-control search-box" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-default">Search</button>
         </form>
-      </ul>
-      <ul class="navbar-nav mr-right>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Add to Cart<span class="sr-only">(current)</span></a>
-      </li>
-      </ul>
-    </div>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="/cartlist">cart({{$total}})</a></li>
+          @if(Session::has('user'))
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="/logout">Logout</a></li>
+            </ul>
+          </li>
+          @else
+          <li><a href="/login">Login</a></li>
+          @endif
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
   </nav>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,51 +1,21 @@
 @extends('master')
-@section('content')
-    
-<div class="container">
+@section("content")
+<div class="container custom-login">
     <div class="row">
-        <div class="col-md-12 min-vh-100 d-flex flex-column justify-content-center">
-            <div class="row">
-                <div class="col-lg-6 col-md-8 mx-auto">
-
-                    <!-- form card login -->
-                    <div class="card rounded shadow shadow-sm">
-                        <div class="card-header">
-                            <h3 class="mb-0">Login</h3>
-                        </div>
-                        <div class="card-body">
-                            <form action="login" class="form" role="form" autocomplete="off" id="formLogin" novalidate="" method="POST">
-                                <div class="form-group">
-                                    @csrf
-                                    <label for="uname1">Username</label>
-                                    <input name="email"type="text" class="form-control form-control-lg rounded-0" name="uname1" id="uname1" required="">
-                                    <div class="invalid-feedback">Oops, you missed this one.</div>
-                                </div>
-                                <div class="form-group">
-                                    <label>Password</label>
-                                    <input name="password"type="password" class="form-control form-control-lg rounded-0" id="pwd1" required="" autocomplete="new-password">
-                                    <div class="invalid-feedback">Enter your password too!</div>
-                                </div>
-                                
-                                <button type="submit" class="btn btn-success btn-lg float-right" id="btnLogin">Login</button>
-                                <a href="#">Forget Password?</a>
-                            </form>
-                        </div>
-                        <!--/card-block-->
-                    </div>
-                    <!-- /form card login -->
-
+        <div class="col-sm-4 col-sm-offset-4">
+            <form action="login" method="POST" >
+                <div class="form-group">
+                    @csrf
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
                 </div>
-
-
-            </div>
-            <!--/row-->
-
+                <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-default">Login</button>
+            </form>
         </div>
-        <!--/col-->
     </div>
-    <!--/row-->
 </div>
-<!--/container-->
-
-
 @endsection
